67ab388bec1426f6cdf9ce9091cdc9f45c579104
marcelbuesing <buesing.marcel@googlemail.com>
2017-03-31 19:13:33 +0200

    Fix record names in test

--- tests/test.hs
+++ tests/test.hs
@@ -25,35 +25,47 @@ main = defaultMain $ testGroup "NMEA"
 
 gpggaTest :: Assertion
 gpggaTest = do
-    let sGpgga = pack "$GPGGA,120000.000,4250.5589,S,14718.5084,E,1,04,24.4,19.7,M,,,,0000*1F"
+    let sGpgga = "$GPGGA,120000.000,4250.5589,S,14718.5084,E,1,04,24.4,19.7,M,,,,0000*1F"
         day' = fromGregorian 1970 1 1
         eGpgga = Gpgga {
-          _gppgaTimeUTC = ZonedTime (LocalTime day' midday) utc
-        , _gppgaLatitude = Latitude {_latitudeValue = 4250.5589, _latitudeDirection = South}
-        , _gppgaLongitude = Longitude {_longitudeValue = 14718.5084, _longitudeDirection = East},
-          _gppgaGpsQuality = GPSFix,
-          _gppgaNumberOfSatellites = 4,
-          _gppgaHorizDilutionOfPrecision = 24.4,
-          _gppgaAltitude = 19.7,
-          _gppgaGeoidalSeparation = 0.0,
-          _gppgaAgeDifferentialGPSData = 0.0,
-          _gppgaDgpsReferenceStation = DGPSReferenceStation {_dgpsReferenceStation = 0}
+          _gpggaTimeUTC = ZonedTime (LocalTime day' midday) utc
+        , _gpggaLatitude = Latitude
+          { _latitudeValue = toDegreesMinutes 4250.5589
+          , _latitudeDirection = South
+          }
+        , _gpggaLongitude = Longitude
+          { _longitudeValue = toDegreesMinutes 14718.5084
+          , _longitudeDirection = East
+          },
+          _gpggaGpsQuality = GPSFix,
+          _gpggaNumberOfSatellites = 4,
+          _gpggaHorizDilutionOfPrecision = 24.4,
+          _gpggaAltitude = 19.7,
+          _gpggaGeoidalSeparation = 0.0,
+          _gpggaAgeDifferentialGPSData = 0.0,
+          _gpggaDgpsReferenceStation = DGPSReferenceStation {_dgpsReferenceStation = 0}
           }
     parseOnly gpgga sGpgga @?= Right eGpgga
 
 gprmcTest :: Assertion
 gprmcTest = do
-    let sGprmc = pack "$GPRMC,000000.000,A,3723.2475,N,12158.3416,W,0.13,309.62,120598,,*10"
+    let sGprmc = "$GPRMC,000000.000,A,3723.2475,N,12158.3416,W,0.13,309.62,120598,0.4,E,A*57"
         day' = fromGregorian 1970 1 1
-        magVariation = MagneticVariation
-          { _magneticVariationValue = Degree {_unDegree = 0.0}
+        magVariation = Just $ MagneticVariation
+          { _magneticVariationValue = Degree {_unDegree = 0.4}
           , _magneticVariationDirection = East
           }
         eGprmc = Gprmc
           { _gprmcTimeUTC = ZonedTime (LocalTime day' midnight) utc
           , _gprmcStatus = DataValid
-          , _gprmcLatitude = Latitude {_latitudeValue = 3723.2475, _latitudeDirection = North}
-          , _gprmcLongitude = Longitude {_longitudeValue = 12158.3416, _longitudeDirection = West}
+          , _gprmcLatitude = Latitude
+            { _latitudeValue = toDegreesMinutes 3723.2475
+            , _latitudeDirection = North
+            }
+          , _gprmcLongitude = Longitude
+            { _longitudeValue = toDegreesMinutes 12158.3416
+            , _longitudeDirection = West
+            }
           , _gprmcSpeedOverGround = Knot {_unKnot = 0.13}
           , _gprmcCourseOverGround = Degree {_unDegree = 309.62}
           , _gpmrcDate = fromGregorian 1998 5 12
@@ -65,20 +77,20 @@ gprmcTest = do
 gphdtTest :: Assertion
 gphdtTest =
   parseOnly gphdt sGphdt @?= Right eGphdt
-  where sGphdt = pack "$GPHDT,175.58,T*0C"
+  where sGphdt = "$GPHDT,175.58,T*0C"
         eGphdt = Gphdt (Degree 175.58)
 
 gpgsaTest :: Assertion
 gpgsaTest =
   parseOnly gpgsa sGpgsa @?= Right eGpgsa
-  where sGpgsa = pack "$GPGSA,A,3,19,28,14,18,27,22,31,39,,,,,1.7,1.0,1.3*35"
+  where sGpgsa = "$GPGSA,A,3,19,28,14,18,27,22,31,39,,,,,1.7,1.0,1.3*35"
         eSats = SatellitePRN <$> [19, 28, 14, 18, 27, 22, 31, 39]
         eGpgsa = Gpgsa Automatic FixNotAvailable eSats (PDOP 1.7) (HDOP 1.0) (VDOP 1.3)
 
 gpgsvTest :: Assertion
 gpgsvTest =
   parseOnly gpgsv input @?= Right expected
-  where input = pack "$GPGSV,3,3,11,22,42,067,42,24,14,311,43,27,05,244,00,,,,*4D"
+  where input = "$GPGSV,3,3,11,22,42,067,42,24,14,311,43,27,05,244,00,,,,*4D"
         satA = SatelliteInView (SatellitePRN 22) (Elevation 42) (Azimuth 67) (SatelliteSNR 42)
         satB = SatelliteInView (SatellitePRN 24) (Elevation 14) (Azimuth 311) (SatelliteSNR 43)
         satC = SatelliteInView (SatellitePRN 27) (Elevation 5) (Azimuth 244) (SatelliteSNR 0)
